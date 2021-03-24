835c316a1602c18425cffe474dd27b2f14cdac4d
Taras <seman6745@gmail.com>
2021-03-24 16:48:49 +0200

    without form mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const submit = document.querySelector('.contact-as__button');
+const formContactUs = document.querySelector('.contact-as__form');
+const formSubmit = document.querySelector('.consultation__form');
+
+document.querySelector('.contact-as__textarea').value = '';
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  document.querySelector('.contact-as__textarea').value = '';
+  formContactUs.reset();
+});
+
+formSubmit.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formSubmit.reset();
+});
